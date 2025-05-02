d036ce2e6788fb91ce516e1ea418f5a1c7d88f2b
HamzaBatta <hamzatb202@gmail.com>
2025-05-03 00:26:32 +0300

    reordering the routes

--- routes/api.php
+++ routes/api.php
@@ -8,46 +8,49 @@ use App\Http\Controllers\MediaController;
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
-
-
-
-
+// {====== Public Routes ======}
 Route::post('/auth/google/token', [GoogleAuthController::class, 'handleGoogleToken']);
-
-
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
-Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:api');
-
-
 
 Route::post('/verify-code', [AuthController::class, 'verifyCode']);
-
-
 Route::post('/request-reset-code', [AuthController::class, 'requestResetCode']);
 Route::post('/verify-reset-code', [AuthController::class, 'verifyResetCode']);
 
-// {======Posts======}
+// {====== Public Read-Only Routes ======}
 Route::get('/posts', [PostController::class, 'index']);
-Route::post('/posts', [PostController::class, 'store']);
 Route::get('/posts/{id}', [PostController::class, 'show']);
-Route::post('/posts/{id}', [PostController::class, 'update']);
-Route::delete('/posts/{id}', [PostController::class, 'destroy']);
 
-// {======Media======}
 Route::get('/posts/{postId}/media', [MediaController::class, 'index']);
-Route::post('/posts/{postId}/media', [MediaController::class, 'store']);
 Route::get('/posts/{postId}/media/{mediaId}', [MediaController::class, 'show']);
-Route::delete('/posts/{postId}/media/{mediaId}', [MediaController::class, 'destroy']);
 
-// {======Comments======}
-Route::get('/comments', [CommentController::class, 'index']); // pass post_id or comment_id in the
-Route::post('/comments', [CommentController::class, 'store']);
+Route::get('/comments', [CommentController::class, 'index']);  // pass post_id or comment_id
 Route::get('/comments/{id}', [CommentController::class, 'show']);
-Route::post('/comments/{id}', [CommentController::class, 'update']);
-Route::delete('/comments/{id}', [CommentController::class, 'destroy']);
 
-Route::post('/likes', [LikeController::class, 'toggle']);
+
+// {====== Protected Routes (auth:api) ======}
+Route::middleware('auth:api')->group(function () {
+
+    // Auth
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+    // Posts
+    Route::post('/posts', [PostController::class, 'store']);
+    Route::post('/posts/{id}', [PostController::class, 'update']);
+    Route::delete('/posts/{id}', [PostController::class, 'destroy']);
+
+    // Media
+    Route::post('/posts/{postId}/media', [MediaController::class, 'store']);
+    Route::delete('/posts/{postId}/media/{mediaId}', [MediaController::class, 'destroy']);
+
+    // Comments
+    Route::post('/comments', [CommentController::class, 'store']);
+    Route::post('/comments/{id}', [CommentController::class, 'update']);
+    Route::delete('/comments/{id}', [CommentController::class, 'destroy']);
+
+    // Likes
+    Route::post('/likes', [LikeController::class, 'toggle']);
+});
 
 
