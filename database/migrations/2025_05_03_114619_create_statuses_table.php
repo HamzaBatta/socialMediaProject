f0582532a1a6d410729c2af33b11add75ad57a4f
yousef1110ya <yousefalshikhali1711@gmail.com>
2025-05-03 15:06:45 +0300

    created the migrations
    took all the details from the hamza/posts and then created a new branch
    named yousuf/migrations and this is the first commit before merging it
    with the main branch

--- database/migrations/2025_05_03_114619_create_statuses_table.php
+++ database/migrations/2025_05_03_114619_create_statuses_table.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -12,13 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('posts', function (Blueprint $table) {
+        Schema::create('statuses', function (Blueprint $table) {
             $table->id();
-            $table->foreignIdFor(User::class)->constrained()->onDelete('cascade');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->text('text')->nullable();
-            $table->foreignId('group_id')->nullable()->constrained()->onDelete('cascade');
-            $table->integer('likes_count')->default(0);
-            $table->integer('comments_count')->default(0);
+            $table->timestamp('expires_at');
+            $table->boolean('is_active')->default(true);
             $table->timestamps();
         });
     }
@@ -28,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('posts');
+        Schema::dropIfExists('statuses');
     }
 };
--- database/migrations/2025_05_03_114808_create_requests_table.php
+++ database/migrations/2025_05_03_114808_create_requests_table.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Models\Post;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -12,10 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('media', function (Blueprint $table) {
+        Schema::create('requests', function (Blueprint $table) {
             $table->id();
-            $table->string('path');
-            $table->enum('type',['image','video']);
+            $table->enum('state', ['pending', 'approved', 'rejected'])->default('pending');
+            $table->timestamp('requested_at');
+            $table->timestamp('responded_at')->nullable();
             $table->timestamps();
         });
     }
@@ -25,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('media');
+        Schema::dropIfExists('requests');
     }
 };
